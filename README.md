ec7f823534da33f6709f08ff5bf98b758c55141b
MBawakid1409 <m.bawakid.1409@gmail.com>
2021-11-09 17:03:58 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# JS_Day_3
+
+## Javascript Coding Bootcamp "Tuwaiq-1000 online"
+
+### Homework of Day 3
---
+++ main.js
@@ -0,0 +1,55 @@
+console.log('Week 1 - Day 3')
+
+console.log('Tuwaiq Academy   JS')
+
+
+/*
+let a = 5;
+let b = a * 2;
+let c = a + b;
+let d = c - a;
+console.log(a, b, c, d);
+
+//The Homework
+let firstVariable = 46 ;
+let secondVariable = firstVariable * 4;
+let thirdVariable = firstVariable + secondVariable;
+let forthVariable = thirdVariable - secondVariable;
+let firstName = 'Mohammad';
+let familyName = 'Bawakid';
+let myAge = 33;
+let phraseResult = `my name is ${firstName} ${familyName}, my age is ${myAge}`;
+let phraseLength = phraseResult.length;
+let phraseIndex = phraseResult[6];
+
+console.log(firstVariable+ '\n', secondVariable+ '\n', thirdVariable+ '\n', forthVariable+ '\n', firstName, familyName+ '\n', myAge+ '\n', phraseResult+ '\n', phraseLength+ '\n', phraseIndex+ '\n');
+*/
+
+//The Homework
+let hw_D03_1 = 1 - 3 + 6 > 1 - 8;
+let hw_D03_2 = 2 * (3 + 9) >= 9 - 1 * 0;
+let hw_D03_3 = (6 / 2) * 5 <= 1 - 5;
+let hw_D03_4 = 15 % 2 > 9 - 1;
+let hw_D03_5 = 7 * 7 === '4' + '9';
+let hw_D03_6 = 7 * 7 == '4' + '9';
+let hw_D03_7 = 7 * 7 !== '4' + '9';
+let hw_D03_8 = 7 * 7 != '4' + '9';
+let hw_D03_9 = 7 > '7';
+let hw_D03_10 = 7 <= '7';
+let hw_D03_11 = 9 + 2 > 8 - 5 * 9;
+
+console.log(hw_D03_1+'\n', hw_D03_2+'\n', hw_D03_3+'\n', hw_D03_4+'\n', hw_D03_5+'\n', hw_D03_6+'\n', hw_D03_7+'\n', hw_D03_8+'\n', hw_D03_9+'\n', hw_D03_10+'\n', hw_D03_11);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
